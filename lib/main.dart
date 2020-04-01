1803c3f9d1a010f52715f3c4f2e1dad3a8252047
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2020-04-01 19:03:38 +0800

    Colours & Fonts (Lesson 6)

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,24 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('My First App'),
       centerTitle: true,
+      backgroundColor: Colors.red[600],
     ),
     body: Center(
-      child: Text('Hello Flutter'),
+      child: Text(
+        'Hello Flutter',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'ComicNeue',
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
+      onPressed: (){},
       child: Text('click'),
+      backgroundColor: Colors.red[600],
     ),
   ),
 
