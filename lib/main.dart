3159128e0b8795d3f43d919c93fbaf593018af79
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2020-04-01 20:20:42 +0800

    Images & Assets (Lesson 8)

--- lib/main.dart
+++ lib/main.dart
@@ -15,16 +15,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          'Hello Flutter',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'ComicNeue',
-          ),
-        ),
+        child: Image.asset('assets/night.jfif'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
