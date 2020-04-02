d7c97cbc130dbf3a2bc4af12a4cb553215926dd6
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2020-04-02 17:59:10 +0800

    Passing Route Data (Lesson 30)

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -10,14 +10,13 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  String time = 'loading';
-
   void setupWorldTime() async{
     WorldTime instance = WorldTime(location: 'Kuala_Lumpur', flag: 'malaysia.png',url: 'Asia/Kuala_Lumpur');
     await instance.getTime();
-    print(instance.time);
-    setState(() {
-      time = instance.time;
+    Navigator.pushReplacementNamed(context, '/home',arguments: {
+      'location' :instance.location,
+      'flag' : instance.flag,
+      'time' : instance.time
     });
   }
 
@@ -33,7 +32,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(50),
-        child: Text(time),
+        child: Text('loading'),
       )
     );
   }
