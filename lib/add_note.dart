8d18281dd9033039f31231458e515687f2d4a911
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2020-04-24 23:00:21 +0800

    add Form and print submitted data

--- lib/add_note.dart
+++ lib/add_note.dart
@@ -6,32 +6,45 @@ class addNote extends StatefulWidget {
 }
 
 class _addNoteState extends State<addNote> {
+
+  final _formKey = GlobalKey<FormState>();
+  String noteText ="";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Add note'),
       ),
-      body: Column(
-        children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Enter your note'
-            ),
-          ),
+      body:
           Center(
-            child: FlatButton.icon(
-                onPressed: null,
-                icon: Icon(
-                  Icons.add,
-                ),
-                label: Text(
-                  'Add',
-                )),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    validator: (value){
+                      if(value.isEmpty){
+                        return 'Please enter some text';
+                      }else{
+                        noteText = value;
+                      }
+                      return null;
+                    },
+                  ),
+                  RaisedButton(onPressed: (){
+                    if(_formKey.currentState.validate()){
+                      print('$noteText');
+                    }
+                  },
+                  child: Text('Add')
+                  ),
+                ],
+              ),
+            )
           ),
-        ],
-      ),
+
+
     );
   }
 }
-
